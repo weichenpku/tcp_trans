30ce269b8d9a220d24fb60ca0fb88285b803b3b9
weichenpku <pkuweichen@gmail.com>
2016-12-10 21:40:07 +0800

    Add files via upload

--- helper/start.sh
+++ helper/start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 interface_server="enp0s31f6"
-interface_router="enp0s31f6"
+interface_router1="enp0s31f6"
+interface_router2="enp0s31f6"
 path="../etc/Resolve.conf"
+target_file="testfile"
+my_file="myfile"
 
 file1="					
-sudo ifconfig ${interface_router} 192.168.1.2 up; 		
+sudo ifconfig ${interface_router1} 192.168.1.2 up; 		
 ./ftp_client 192.168.1.1 ${path} ${path};
 "
 
 
 file2="
-sudo ifconfig ${interface_router} 192.168.1.3 up;
+sudo ifconfig ${interface_router2} 192.168.1.3 up;
 ./ftp_client 192.168.1.1 ${path} ${path};
 "
 
@@ -26,13 +29,13 @@ bye
 "
 
 file5="
-rm ftp_client ftp_server name* router* *file
+rm ftp_client ftp_server name* router* ${target_file} ${my_file}
 "
 
 echo ${file1} >> router1.sh
 echo ${file2} >> router2.sh
 echo ${file3} >> nameserver.sh
-echo ${file4} >> testfile
+echo ${file4} >> ${target_file}
 echo ${file5} >> stop.sh
 
 chmod 711 *.sh
